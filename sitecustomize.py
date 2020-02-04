35d1ba4f978b833af3eaa34e5700efcd4a4977d5
Bart Oldeman <bart.oldeman@calculquebec.ca>
2020-02-04 02:27:02 +0000

    Even simpler PYTHONPATH for eb without need for sitecustomize.py

--- sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import site
-site.addsitedir('/cvmfs/soft.computecanada.ca/easybuild/easyconfigs')
-site.addsitedir('/cvmfs/soft.computecanada.ca/easybuild/easyblocks')
-site.addsitedir('/cvmfs/soft.computecanada.ca/easybuild/framework')
