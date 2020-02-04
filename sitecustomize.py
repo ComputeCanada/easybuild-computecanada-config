866b58892d432f76b61f810a33b2ff0b93b63037
Bart Oldeman <bart.oldeman@calculquebec.ca>
2020-02-04 01:30:59 +0000

    Remove redundant unset CPATH, simplify PYTHONPATH
    PYTHONPATH now goes via sitecustomize.py.

---
+++ sitecustomize.py
@@ -0,0 +1,4 @@
+import site
+site.addsitedir('/cvmfs/soft.computecanada.ca/easybuild/easyconfigs')
+site.addsitedir('/cvmfs/soft.computecanada.ca/easybuild/easyblocks')
+site.addsitedir('/cvmfs/soft.computecanada.ca/easybuild/framework')
