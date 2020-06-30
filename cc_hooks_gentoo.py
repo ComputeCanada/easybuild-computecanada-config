eb2f5ee128e74ff8153bcb03ac3e7140ba71d6e3
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2020-06-30 16:24:04 +0000

    Moved common functions to cc_hooks_common.py

--- cc_hooks_gentoo.py
+++ cc_hooks_gentoo.py
@@ -2,6 +2,7 @@ from easybuild.framework.easyconfig.easyconfig import get_easyblock_class
 from easybuild.easyblocks.generic.cmakemake import CMakeMake
 from easybuild.framework.easyconfig.constants import EASYCONFIG_CONSTANTS
 from distutils.version import LooseVersion
+from cc_hooks_common import package_match, map_dependency_version, replace_dependencies, modify_dependencies
 
 import os
 
@@ -190,105 +191,6 @@ buildopts_changes = {
                     'avx512': 'TARGET=SKYLAKEX'}[os.getenv('RSNT_ARCH')] + ' NUM_THREADS=64'
 }
 
-def package_match(ref, test):
-    #print("Testing %s against %s" % (str(test),str(ref)))
-    if isinstance(ref,str): ref = (ref, "ANY", "ANY")
-    if isinstance(test,list) and isinstance(test[0],tuple): return False  # does not change anything for multi_deps
-    ref_name = ref[0]
-    ref_version = ref[1]
-    ref_version_suffix = "ANY"
-    if len(ref) >= 3:
-        ref_version_suffix = ref[2]
-
-    test_name = test[0]
-    test_version = test[1]
-    test_version_suffix = None
-    if len(test) >= 3:
-        test_version_suffix = test[2]
-
-    # test name
-    if ref_name != test_name: return False
-    #print("Name matches")
-
-    # test version
-    if ref_version != "ANY":
-        if isinstance(ref_version, str) and test_version != ref_version:
-            return False
-        if isinstance(ref_version, tuple) and not test_version in ref_version:
-            return False
-    #print("Version matches")
-
-    # if we get to this point, the versions match, test version_suffixes
-    if ref_version_suffix != "ANY":
-        # undefined version_suffix required and version_suffix provided
-        if not ref_version_suffix and test_version_suffix:
-            return False
-        if isinstance(ref_version_suffix, str) and test_version_suffix != ref_version_suffix:
-            return False
-        if isinstance(ref_version_suffix, tuple) and not test_version_suffix in ref_version_suffix:
-            return False
-    #print("Version suffix matches")
-
-    return True
-
-def map_dependency_version(dep, new_dep, tc_mapping, mytc):
-    if isinstance(dep,tuple): dep = list(dep)
-    # ensure that it has a length of 4
-    for _ in range(len(dep),4):
-        dep.append(None)
-
-    # figure out what is the right toolchain to put there
-    new_tc = None
-    if 'ALL' in tc_mapping: new_tc = tc_mapping['ALL']
-    for tcs in tc_mapping.keys():
-        if not isinstance(tcs,tuple): continue
-        if mytc == tcs or mytc in tcs:
-            new_tc = tc_mapping[tcs]
-
-    if isinstance(new_dep, str): dep[1] = new_dep
-    if isinstance(new_dep, tuple):
-        dep[1] = new_dep[0]
-        dep[2] = new_dep[1]
-        if len(new_dep) == 3:
-            new_tc = new_dep[2]
-
-    dep[3] = new_tc
-    return dep
-
-def replace_dependencies(ec, tc, param, deps_mapping):
-    mytc = (ec.toolchain.name, ec.toolchain.version)
-    #print("mytc: %s, tc: %s" % (str(mytc), str(tc)))
-    if tc == 'ALL' or mytc == tc or mytc in tc:
-        #print("toolchain match")
-        for n, dep in enumerate(ec[param]):
-            dep = list(dep)
-            if dep[0] == ec.name:
-                print("Dependency has the same name as the easyconfig, not replacing.")
-                continue
-            for new_dep in deps_mapping['pkg_mapping']:
-                new_dep_version = deps_mapping['pkg_mapping'][new_dep]
-                if package_match(new_dep, dep):
-                    print("Dependency %s matches %s" % (str(dep),(new_dep)))
-                    dep = map_dependency_version(dep,new_dep_version,deps_mapping['tc_mapping'],mytc)
-                    print("New dependency: %s" % str(dep))
-                    dep = tuple(dep)
-                    ec[param][n] = dep
-
-def modify_dependencies(ec,param):
-    for tc in new_version_mapping:
-        deps_mapping = new_version_mapping[tc]
-        if tc == 'ALL':
-            replace_dependencies(ec,'ALL',param,deps_mapping)
-        else:
-            replace_dependencies(ec,tc,param,deps_mapping)
-
-    for names in new_version_mapping_app_specific:
-        mapping = new_version_mapping_app_specific[names]
-        if ec['name'] in names:
-            print("Specific dependency mappings exist for %s, applying them" % ec['name'])
-            for tc in mapping:
-                deps_mapping = mapping[tc]
-                replace_dependencies(ec,tc,param,deps_mapping)
 
 def prepend_configopts(ec,changes,key="configopts"):
     print("Changing configopts from: %s" % ec[key])
