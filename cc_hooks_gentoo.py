a52df775e3dfca2e8274bda511ad2414a69f3862
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2021-01-13 16:48:49 +0000

    added EBROOTSZIP and EBROOTGLIB to the libraries that are in gentoo

--- cc_hooks_gentoo.py
+++ cc_hooks_gentoo.py
@@ -705,7 +705,7 @@ def post_module_hook(self, *args, **kwargs):
 def pre_prepare_hook(self, *args, **kwargs):
     packages_in_gentoo = ["EBROOTLIBXML2", "EBROOTLIBJPEGMINTURBO", "EBROOTLIBPNG", "EBROOTLIBTIFF", "EBROOTZLIB",
                           "EBROOTLIBGLU", "EBROOTMESA", "EBROOTFLTK", "EBROOTTCL", "EBROOTTK", "EBROOTBZIP2",
-                          "EBROOTZSTD", "EBROOTFREETYPE"]
+                          "EBROOTZSTD", "EBROOTFREETYPE", "EBROOTGLIB", "EBROOTSZIP"]
     ebrootgentoo = os.environ["EBROOTGENTOO"]
     for package in packages_in_gentoo:
         setvar(package, ebrootgentoo)
