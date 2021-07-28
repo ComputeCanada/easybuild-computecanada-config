5814643f211d955852c37c20c7bb525848cf056e
Bart Oldeman <bart.oldeman@calculquebec.ca>
2021-07-28 11:16:40 +0000

    Remove GCC 10.3->10.2 rules.
    Better to just install 10.3 properly to avoid confusion

--- cc_hooks_gentoo.py
+++ cc_hooks_gentoo.py
@@ -41,8 +41,6 @@ new_version_mapping_2020a = {
         ('FFTW', 'ANY', ""): ('3.3.8', COMPILERS_2020a),
         ('FFTW','ANY','-mpi'): ('3.3.8', cOMPI_2020a),
         'Eigen': ('3.3.7', SYSTEM),
-        ('GCCcore', '10.3.0'): ('10.2.0', SYSTEM),
-        ('GCC', '10.3.0'): ('10.2.0', SYSTEM),
 #        'GDAL': ('3.0.4', COMPILERS_2020a, None),
 #        'GEOS': ('3.8.1', GCCCORE93, None),
         'GObject-Introspection': ('1.64.0', SYSTEM, None),
