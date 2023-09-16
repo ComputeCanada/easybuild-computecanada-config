74231111c7d9750989fab8319454f31798db4615
Bart Oldeman <bart.oldeman@calculquebec.ca>
2023-09-16 00:14:19 +0000

    Correct iompic SUBTOOLCHAIN,  COMPILER_MODULE_NAME
    by calling the iompi logic and deriving from that

--- toolchains/iompic.py
+++ toolchains/iompic.py
@@ -1,5 +1,5 @@
 ##
-# Copyright 2013-2022 Ghent University
+# Copyright 2013-2023 Ghent University
 #
 # This file is part of EasyBuild,
 # originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
@@ -8,7 +8,7 @@
 # Flemish Research Foundation (FWO) (http://www.fwo.be/en)
 # and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
 #
-# http://github.com/hpcugent/easybuild
+# https://github.com/easybuilders/easybuild
 #
 # EasyBuild is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -23,17 +23,36 @@
 # along with EasyBuild.  If not, see <http://www.gnu.org/licenses/>.
 ##
 """
-EasyBuild support for iompic compiler toolchain (includes iccifort, OpenMPI and CUDA).
+EasyBuild support for iompic compiler toolchain (includes Intel compilers (icc, ifort), OpenMPI and CUDA).
 
-:author: Bart Oldeman (Compute Canada)
+Authors:
+
+* Stijn De Weirdt (Ghent University)
+* Kenneth Hoste (Ghent University)
+* Bart Oldeman (Digital Research Alliance of Canada)
 """
 
 from easybuild.toolchains.iompi import Iompi
 from easybuild.toolchains.iccifortcuda import IccIfortCUDA
 from easybuild.toolchains.intelcompilerscuda import IntelCompilersCUDA
+from easybuild.toolchains.intel_compilers import IntelCompilersToolchain
 
 
 class Iompic(Iompi, IccIfortCUDA, IntelCompilersCUDA):
     """Compiler toolchain with Intel compilers (icc/ifort), OpenMPI and CUDA."""
+    # compiler-only subtoolchain can't be determine statically
+    # since depends on toolchain version (see below),
+    # so register both here as possible alternatives (which is taken into account elsewhere)
     NAME = 'iompic'
-    SUBTOOLCHAIN = [IccIfortCUDA.NAME, IntelCompilersCUDA.NAME]
+    SUBTOOLCHAIN = [(IccIfortCUDA.NAME, IntelCompilersCUDA.NAME)]
+
+    def __init__(self, *args, **kwargs):
+        """Constructor for Iompic toolchain class."""
+
+        super(Iompic, self).__init__(*args, **kwargs)
+        if self.SUBTOOLCHAIN == IntelCompilersToolchain.NAME:
+            self.SUBTOOLCHAIN = IntelCompilersCUDA.NAME
+            self.COMPILER_MODULE_NAME = IntelCompilersCUDA.COMPILER_MODULE_NAME
+        else:
+            self.SUBTOOLCHAIN = IccIfortCUDA.NAME
+            self.COMPILER_MODULE_NAME = IccIfortCUDA.COMPILER_MODULE_NAME
