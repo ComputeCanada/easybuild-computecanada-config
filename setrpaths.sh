537901e9d61a2dc693cebd5436c18caf082dc26e
Bart Oldeman <bart.oldeman@calculquebec.ca>
2026-01-23 18:54:04 +0000

    Rewrite setrpaths.sh in python
    This makes it 5 times faster by avoiding many system calls and
    "file -b". Instead it uses pyelftools to see what type of ELF
    object we are dealing with and only calls patchelf when modifying
    (the vast majority of files is not).

--- setrpaths.sh
+++ setrpaths.sh
@@ -1,162 +1,161 @@
-#!/bin/bash
+#!/usr/bin/env python
 
-function print_usage {
-	echo "Usage: $0 --path <path to search> [--add_origin] [--add_path=<path>] [--any_interpreter]"
-}
-
-function patch_rpath {
-	local filename=${1?No filename given}
-	local rpath=''
-	local filetype=$(file -b $filename)
-	local REX_DYNAMIC="^ELF 64-bit LSB.*dynamically linked.*"
-	local REX_SO="^ELF 64-bit LSB shared object.*x86-64.*"
-	local REX_OS_INTERPRETER=".*interpreter /lib64/ld-linux-x86-64.so.2.*"
-	local REX_LINUX_INTERPRETER=".*interpreter.*ld-linux-x86-64.so.2"
-	local REX_LSB_INTERPRETER=".*interpreter.*ld-lsb-x86-64.so.3"
-
-	if [[ -n "$NIXUSER_PROFILE" ]]; then
-		PREFIX=$NIXUSER_PROFILE
-		INTERPRETER=$PREFIX/lib/ld-linux-x86-64.so.2
-		FORCE_RPATH=""
-	elif [[ -n "$EPREFIX" ]]; then
-		PREFIX=$EPREFIX
-		if [[ $EBVERSIONGENTOO -ge 2023 ]]; then
-			INTERPRETER=$PREFIX/lib64/ld-linux-x86-64.so.2
-		else
-			INTERPRETER=$PREFIX/lib/ld-linux-x86-64.so.2
-		fi
-		PREFIX=$EPREFIX
-		FORCE_RPATH="--force-rpath"
-	else
-		echo "Neither nixpkgs nor gentoo modules are loaded. Aborting"
-		exit 1
-	fi
-
-	if [[ $filetype =~ $REX_DYNAMIC ]]; then
-		if [[ $filetype =~ $REX_OS_INTERPRETER ]]; then
-			patchelf --set-interpreter "$INTERPRETER" "$filename"
-			rpath='set'
-		elif [[ $ARG_ANY_INTERPRETER -eq 1 && ( $filetype =~ $REX_LINUX_INTERPRETER || $filetype =~ $REX_LSB_INTERPRETER ) ]]; then
-			patchelf --set-interpreter "$INTERPRETER" "$filename"
-			rpath='set'
-		fi
-	fi
-
-	if [[ $filetype =~ $REX_SO && ("$ARG_ADD_ORIGIN" == "1" || -n "$ARG_ADD_PATH") ]] ; then
-		if ! ldd $filename | grep 'statically linked' > /dev/null; then
-		    rpath='set'
-		fi
-	fi
-
-	if [[ -n "$rpath" && ("$ARG_ADD_ORIGIN" == "1" || -n "$ARG_ADD_PATH") ]] ; then
-		rpath=$(patchelf --print-rpath "$filename")
-		rpath_old=$rpath
-
-		if [[ "$ARG_ADD_ORIGIN" == "1" && "${rpath##\$ORIGIN}" == "$rpath" ]]; then
-			if [ -n "$rpath" ] ; then
-				rpath='$ORIGIN:'$rpath
-			else
-				rpath='$ORIGIN'
-			fi
-		fi
-
-		if [[ -n "$ARG_ADD_PATH" ]]; then
-			if [ -n "$rpath" ] ; then
-				rpath="$ARG_ADD_PATH:"$rpath
-			else
-				rpath="$ARG_ADD_PATH"
-			fi
-		fi
-
-		if [ "$rpath" != "$rpath_old" ] ; then
-			patchelf $FORCE_RPATH --set-rpath "$rpath" "$filename"
-		fi
-	fi
-}
-
-function patch_zip {
-	local filename=${1?Missing filename}
-	local tmp=$(mktemp --directory)
-	local fullname=$(readlink -f $filename)
-
-	cd $tmp
-
-	# Extract all and patch every binary file, and update the archive
-	unzip -q $fullname
-	for fname in $(find . -type f); do
-	        oldperm=$(stat --format %a $fname)
-	        chmod u+w $fname
-		patch_rpath $fname;
-		chmod $oldperm $fname
-	done
-	zip -rq $fullname .
-
-	cd -
-	rm -rf $tmp
-}
-
-TEMP=$(getopt -o p: --longoptions path:,add_origin,add_path:,any_interpreter -n $0 -- "$@")
-eval set -- "$TEMP"
-ARG_ADD_ORIGIN=0
-ARG_ADD_PATH=
-ARG_ANY_INTERPRETER=0
-
-while true; do
-	case "$1" in
-		-p|--path)
-			case "$2" in
-				"") ARG_PATH=""; shift 2 ;;
-				*) ARG_PATH=$2; shift 2 ;;
-			esac ;;
-		--add_origin)
-			ARG_ADD_ORIGIN=1; shift ;;
-		--any_interpreter)
-			ARG_ANY_INTERPRETER=1; shift ;;
-		--add_path)
-			case "$2" in
-				"") ARG_ADD_PATH=""; shift 2 ;;
-				*) ARG_ADD_PATH=$2; shift 2 ;;
-			esac ;;
-		--) shift; break ;;
-		*) echo "Unknown parameter $1"; print_usage; exit 1 ;;
-	esac
-done
-
-if [[ -z "$ARG_PATH" ]]; then
-	print_usage; exit 1
-fi
-
-# Avoid failing to set rpaths when a symlink is provided
-if [[ -L "$ARG_PATH" ]]; then
-	echo "error: $ARG_PATH is a symlink. Please provide a path not a symlink."
-	exit 1
-fi
-
-SAVEIFS=$IFS
-IFS=$(echo -en "\n\b")
-for filename in $(find $ARG_PATH -type f -print0 | python -c '
-# quick pre-filter to cut down on "file -b" overhead
+import getopt
+import os
+import re
+import stat
+import subprocess
 import sys
-sys.stdout.reconfigure(line_buffering=True)
+import tempfile
+from pathlib import Path
+from elftools.common.exceptions import ELFError
+from elftools.elf.dynamic import DynamicSection
+from elftools.elf.elffile import ELFFile
 
-for f in sys.stdin.buffer.read()[:-1].split(b"\0"):
-    header = b"\x7fELF"
-    if f[-4:] not in {b".whl", b".jar"}:
-        with open(f, "rb") as myfile:
-            header = myfile.read(4)
-    if header == b"\x7fELF":
-        sys.stdout.buffer.write(f+b"\n")
-'); do
-	if [[ -z "$filename" ]]; then
-		continue
-	fi
-	if [[ $filename == *.jar ]]; then
-		patch_zip $filename
-	elif [[ $filename == *.whl ]]; then
-		patch_zip $filename
-	else
-		patch_rpath $filename
-	fi
-done
-IFS=$SAVEIFS
-exit 0
+REX_OS_INTERPRETER = "/lib64/ld-linux-x86-64.so.2.*"
+REX_LINUX_INTERPRETER = ".*ld-linux-x86-64.so.2"
+REX_LSB_INTERPRETER = ".*ld-lsb-x86-64.so.3"
+
+def print_usage():
+    print("Usage: $0 --path <path to search> [--add_origin] [--add_path=<path>] [--any_interpreter]")
+
+def patch_rpath(filename, arg_add_origin, arg_any_interpreter, arg_add_path,
+                interpreter, force_rpath):
+
+    set_interp = False
+    has_needed = False
+    interp_name = ""
+    with open(filename, "rb") as f:
+        try:
+            elf = ELFFile(f)
+            if elf.header['e_type'] not in {'ET_DYN', 'ET_EXEC'}:
+                return
+            for seg in elf.iter_segments('PT_INTERP'):
+                interp_name = seg.get_interp_name()
+            # check if shared libraries have NEEDED entries, if not
+            # we whould not manipulate RPATH
+            if not interp_name and (arg_add_origin or arg_add_path):
+                for section in elf.iter_sections():
+                    if isinstance(section, DynamicSection):
+                        for tag in section.iter_tags():
+                            if tag.entry.d_tag == 'DT_NEEDED':
+                                has_needed = True
+                                break
+        except ELFError:
+            return
+
+    if interp_name:
+        if (re.match(REX_OS_INTERPRETER, interp_name) or
+            (arg_any_interpreter and (re.match(REX_LINUX_INTERPRETER, interp_name) or
+                                      re.match(REX_LSB_INTERPRETER, interp_name)))):
+            subprocess.run(["patchelf", "--set-interpreter", interpreter, filename])
+            set_interp = True
+
+    if (arg_add_origin or arg_add_path) and (set_interp or has_needed):
+        rpath = subprocess.check_output(["patchelf", "--print-rpath", filename])
+        rpath_old = rpath
+
+        if arg_add_origin and not rpath.startswith(b"$ORIGIN"):
+            if rpath.strip():
+                rpath = b'$ORIGIN:' + rpath
+            else:
+                rpath = b'$ORIGIN'
+
+        if arg_add_path:
+            if rpath:
+                rpath = arg_add_path + b":" + rpath
+            else:
+                rpath = arg_add_path
+
+        if rpath != rpath_old:
+            if force_rpath:
+                subprocess.run(["patchelf", "--force-rpath", "--set-rpath", rpath, filename])
+            else:
+                subprocess.run(["patchelf", "--set-rpath", rpath, filename])
+
+
+def patch_zip(filename, arg_add_origin, arg_any_interpreter, arg_add_path,
+              interpreter, force_rpath):
+    fullname = os.path.realpath(filename)
+    oldcwd = os.getcwd()
+    with tempfile.TemporaryDirectory() as tmp:
+        os.chdir(tmp)
+
+        # Extract all and patch every binary file, and update the archive
+        subprocess.run(["unzip", "-q", fullname])    
+        for root, dirs, files in os.walk(os.getcwd()):
+            root = Path(root)
+            for name in files:
+                fname = root / name
+                oldperm = os.stat(fname).st_mode
+                os.chmod(fname, oldperm | stat.S_IWUSR)
+                patch_rpath(fname, arg_add_origin, arg_any_interpreter, arg_add_path,
+                            interpreter, force_rpath)
+                os.chmod(fname, oldperm)
+
+        subprocess.run(["zip", "-rq", fullname, "."])
+        os.chdir(oldcwd)
+        print(oldcwd)
+
+
+def main():
+    optlist, _ = getopt.gnu_getopt(sys.argv, 'p:', ["path=", "add_origin" , "add_path=",
+                                                    "any_interpreter"])
+    arg_add_origin = False
+    arg_add_path = ""
+    arg_any_interpreter = False
+    arg_path = ""
+
+    for opt, value in optlist:
+        if opt in ('-p', '--path'):
+            arg_path = value
+        elif opt == '--add_origin':
+            arg_add_origin = True
+        elif opt == '--any_interpreter':
+            arg_any_interpreter = True
+        elif opt == '--add_path':
+            arg_add_path = value
+        else:
+            print(f"Unknown parameter {opt}")
+            print_usage()
+            return 1
+
+    if not arg_path:
+        print_usage()
+        return 1
+
+    # Avoid failing to set rpaths when a symlink is provided
+    if stat.S_ISLNK(os.stat(arg_path).st_mode):
+        print(f"error: {arg_path} is a symlink. Please provide a path not a symlink.")
+        return 1
+
+    if "NIXUSER_PROFILE" in os.environ:
+        prefix = Path(os.environ["NIXUSER_PROFILE"])
+        interpreter = prefix / "lib" / "ld-linux-x86-64.so.2"
+        force_rpath = False
+    elif "EPREFIX" in os.environ:
+        prefix = Path(os.environ["EPREFIX"])
+        year = int(os.environ["EBVERSIONGENTOO"])
+        if year >= 2023:
+            interpreter = prefix / "lib64" / "ld-linux-x86-64.so.2"
+        else:
+            interpreter = prefix / "lib" / "ld-linux-x86-64.so.2"
+        force_rpath = True
+    else:
+        print("Neither nixpkgs nor gentoo modules are loaded. Aborting")
+        sys.exit(1)
+
+    for root, dirs, files in os.walk(arg_path):
+        root = Path(root)
+        for name in files:
+            f = root / name
+            if name[-4:] in {".whl", ".jar"}:
+                patch_zip(f, arg_add_origin, arg_any_interpreter, arg_add_path,
+                          interpreter, force_rpath)
+            else:
+                patch_rpath(f, arg_add_origin, arg_any_interpreter, arg_add_path,
+                            interpreter, force_rpath)
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
