05f90c3728f0ddbf0cc911f6a70edb3c8a271cea
Bart Oldeman <bart.oldeman@calculquebec.ca>
2026-01-26 13:14:14 +0000

    Use argparse, as suggested by @ccoulombe

--- setrpaths.sh
+++ setrpaths.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import getopt
+import argparse
 import os
 import re
 import stat
@@ -16,11 +16,7 @@ REX_OS_INTERPRETER = "/lib64/ld-linux-x86-64.so.2.*"
 REX_LINUX_INTERPRETER = ".*ld-linux-x86-64.so.2"
 REX_LSB_INTERPRETER = ".*ld-lsb-x86-64.so.3"
 
-def print_usage():
-    print("Usage: $0 --path <path to search> [--add_origin] [--add_path=<path>] [--any_interpreter]")
-
-def patch_rpath(filename, arg_add_origin, arg_any_interpreter, arg_add_path,
-                interpreter, force_rpath):
+def patch_rpath(filename, args, interpreter, force_rpath):
 
     set_interp = False
     has_needed = False
@@ -34,7 +30,7 @@ def patch_rpath(filename, arg_add_origin, arg_any_interpreter, arg_add_path,
                 interp_name = seg.get_interp_name()
             # check if shared libraries have NEEDED entries, if not
             # we whould not manipulate RPATH
-            if not interp_name and (arg_add_origin or arg_add_path):
+            if not interp_name and (args.add_origin or args.add_path):
                 for section in elf.iter_sections():
                     if isinstance(section, DynamicSection):
                         for tag in section.iter_tags():
@@ -46,26 +42,26 @@ def patch_rpath(filename, arg_add_origin, arg_any_interpreter, arg_add_path,
 
     if interp_name:
         if (re.match(REX_OS_INTERPRETER, interp_name) or
-            (arg_any_interpreter and (re.match(REX_LINUX_INTERPRETER, interp_name) or
-                                      re.match(REX_LSB_INTERPRETER, interp_name)))):
+            (args.any_interpreter and (re.match(REX_LINUX_INTERPRETER, interp_name) or
+                                       re.match(REX_LSB_INTERPRETER, interp_name)))):
             subprocess.run(["patchelf", "--set-interpreter", interpreter, filename])
             set_interp = True
 
-    if (arg_add_origin or arg_add_path) and (set_interp or has_needed):
+    if (args.add_origin or args.add_path) and (set_interp or has_needed):
         rpath = subprocess.check_output(["patchelf", "--print-rpath", filename])
         rpath_old = rpath
 
-        if arg_add_origin and not rpath.startswith(b"$ORIGIN"):
+        if args.add_origin and not rpath.startswith(b"$ORIGIN"):
             if rpath.strip():
                 rpath = b'$ORIGIN:' + rpath
             else:
                 rpath = b'$ORIGIN'
 
-        if arg_add_path:
+        if args.add_path:
             if rpath:
-                rpath = arg_add_path + b":" + rpath
+                rpath = args.add_path + b":" + rpath
             else:
-                rpath = arg_add_path
+                rpath = args.add_path
 
         if rpath != rpath_old:
             if force_rpath:
@@ -74,8 +70,7 @@ def patch_rpath(filename, arg_add_origin, arg_any_interpreter, arg_add_path,
                 subprocess.run(["patchelf", "--set-rpath", rpath, filename])
 
 
-def patch_zip(filename, arg_add_origin, arg_any_interpreter, arg_add_path,
-              interpreter, force_rpath):
+def patch_zip(filename, args, interpreter, force_rpath):
     fullname = os.path.realpath(filename)
     oldcwd = os.getcwd()
     with tempfile.TemporaryDirectory() as tmp:
@@ -89,8 +84,7 @@ def patch_zip(filename, arg_add_origin, arg_any_interpreter, arg_add_path,
                 fname = root / name
                 oldperm = os.stat(fname).st_mode
                 os.chmod(fname, oldperm | stat.S_IWUSR)
-                patch_rpath(fname, arg_add_origin, arg_any_interpreter, arg_add_path,
-                            interpreter, force_rpath)
+                patch_rpath(fname, args, interpreter, force_rpath)
                 os.chmod(fname, oldperm)
 
         subprocess.run(["zip", "-rq", fullname, "."])
@@ -99,34 +93,20 @@ def patch_zip(filename, arg_add_origin, arg_any_interpreter, arg_add_path,
 
 
 def main():
-    optlist, _ = getopt.gnu_getopt(sys.argv, 'p:', ["path=", "add_origin" , "add_path=",
-                                                    "any_interpreter"])
-    arg_add_origin = False
-    arg_add_path = ""
-    arg_any_interpreter = False
-    arg_path = ""
-
-    for opt, value in optlist:
-        if opt in ('-p', '--path'):
-            arg_path = value
-        elif opt == '--add_origin':
-            arg_add_origin = True
-        elif opt == '--any_interpreter':
-            arg_any_interpreter = True
-        elif opt == '--add_path':
-            arg_add_path = value
-        else:
-            print(f"Unknown parameter {opt}")
-            print_usage()
-            return 1
-
-    if not arg_path:
-        print_usage()
-        return 1
+    parser = argparse.ArgumentParser(
+        description='Links Linux binaries to libraries in the current Gentoo Prefix')
+    parser.add_argument('-p', '--path', type=str, required=True, help="Path to recursively descend",
+                        metavar='<path to search>')
+    parser.add_argument('--add_origin', action='store_true', help="Add $ORIGIN to RPATH")
+    parser.add_argument('--add_path', type=str, help="RPATH to add",
+                        metavar='<path>')
+    parser.add_argument('--any_interpreter', action='store_true',
+                        help="Patch any interpreter, not only the system one")
+    args = parser.parse_args()
 
     # Avoid failing to set rpaths when a symlink is provided
-    if stat.S_ISLNK(os.stat(arg_path).st_mode):
-        print(f"error: {arg_path} is a symlink. Please provide a path not a symlink.")
+    if stat.S_ISLNK(os.stat(args.path).st_mode):
+        print(f"error: {args.path} is a symlink. Please provide a path not a symlink.")
         return 1
 
     if "NIXUSER_PROFILE" in os.environ:
@@ -145,16 +125,14 @@ def main():
         print("Neither nixpkgs nor gentoo modules are loaded. Aborting")
         sys.exit(1)
 
-    for root, dirs, files in os.walk(arg_path):
+    for root, dirs, files in os.walk(args.path):
         root = Path(root)
         for name in files:
             f = root / name
             if name[-4:] in {".whl", ".jar"}:
-                patch_zip(f, arg_add_origin, arg_any_interpreter, arg_add_path,
-                          interpreter, force_rpath)
+                patch_zip(f, args, interpreter, force_rpath)
             else:
-                patch_rpath(f, arg_add_origin, arg_any_interpreter, arg_add_path,
-                            interpreter, force_rpath)
+                patch_rpath(f, args, interpreter, force_rpath)
     return 0
 
 if __name__ == "__main__":
