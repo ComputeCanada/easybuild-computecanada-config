548b4af5b217527f99bd1f328e6aab33a526d7da
Bart Oldeman <bart.oldeman@calculquebec.ca>
2026-03-03 23:32:41 +0000

    Don't use pathlib any more for bwrapcmd
    Using older ways is easier to sync with upstream method

--- bwrapcmd.py
+++ bwrapcmd.py
@@ -1,5 +1,4 @@
 import sys, os
-from pathlib import Path
 from easybuild.framework.easyconfig.easyconfig import process_easyconfig
 from easybuild.tools.options import set_up_configuration
 
@@ -7,8 +6,8 @@ if __name__ == "__main__":
     os.environ["EASYBUILD_LOGTOSTDOUT"] = "1"
     os.environ["EASYBUILD_CONFIGFILES"] = os.environ["LOCAL_EASYBUILD_CONFIGFILES"]
     os.environ["EASYBUILD_ROBOT_PATHS"] = os.pathsep.join((os.environ["EASYBUILD_ROBOT_PATHS"],
-                                                           str(Path(os.environ["EBROOTGENTOO"]) /
-                                                               "easybuild" / "easyconfigs")))
+                                                           os.path.join(os.environ["EBROOTGENTOO"],
+                                                                        "easybuild", "easyconfigs")))
 
     old_stdout = sys.stdout
     sys.stdout = open(os.devnull, "w")
@@ -18,19 +17,17 @@ if __name__ == "__main__":
 
     sys.stdout.close()
     full_mod_name = parsed_ec[0]['full_mod_name']
-    prefix = Path(os.environ.get("EASYBUILD_PREFIX", "/cvmfs/soft.computecanada.ca/easybuild"))
-    eis = Path(os.environ.get("EASYBUILD_INSTALLPATH_SOFTWARE", prefix / os.environ["EASYBUILD_SUBDIR_SOFTWARE"]))
-    eim = prefix / os.environ["EASYBUILD_SUBDIR_MODULES"]
-    spath = eis / full_mod_name
-    mpath = eim / full_mod_name
-    for i, p in enumerate(spath.parents):
-        if p.exists():
-            spath = p
-            mpath = mpath.parents[i]
-            break
+    prefix = os.environ.get("EASYBUILD_PREFIX", "/cvmfs/soft.computecanada.ca/easybuild")
+    eis = os.environ.get("EASYBUILD_INSTALLPATH_SOFTWARE", os.path.join(prefix, os.environ["EASYBUILD_SUBDIR_SOFTWARE"]))
+    eim = os.path.join(prefix, os.environ["EASYBUILD_SUBDIR_MODULES"])
+    spath = os.path.dirname(os.path.join(eis, full_mod_name))
+    mpath = os.path.dirname(os.path.join(eim, full_mod_name))
+    while not os.path.exists(spath):
+        spath = os.path.dirname(spath)
+        mpath = os.path.dirname(mpath)
 
-    bwrapdir = Path(sys.argv[2])
-    (bwrapdir / spath.relative_to('/')).mkdir(parents=True, exist_ok=True)
-    (bwrapdir / mpath.relative_to('/')).mkdir(parents=True, exist_ok=True)
+    bwrapdir = sys.argv[2]
+    os.makedirs(os.path.join(bwrapdir, spath), exist_ok=True)
+    os.makedirs(os.path.join(bwrapdir, mpath), exist_ok=True)
     bwrap_cmd = f"bwrap --dev-bind / / --bind {bwrapdir}{spath} {spath} --bind {bwrapdir}{mpath} {mpath}"
     old_stdout.write(f"{bwrap_cmd}\n")
