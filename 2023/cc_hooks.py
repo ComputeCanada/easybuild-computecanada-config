ce870c9e5f159c9d5c119b5e87d211a25d69b911
Bart Oldeman <bart.oldeman@calculquebec.ca>
2026-03-17 15:30:07 +0000

    Fix path for intel mpi in setrpaths.sh call in hooks

--- 2023/cc_hooks.py
+++ 2023/cc_hooks.py
@@ -224,8 +224,8 @@ intelmpi2021_dict = {
         "for i in %(installdir)s/mpi/latest/bin/I*; do patchelf --set-rpath '$ORIGIN/../lib/release' --force-rpath $i; done",
         "patchelf --set-rpath '$ORIGIN/../lib/release:$ORIGIN/../libfabric/lib' --force-rpath %(installdir)s/mpi/latest/bin/impi_info",
         "if [ -L %(installdir)s/mpi/latest/lib/release ]; then "
-        "  for f in %(installdir)s/mpi/latest/lib/*.so.*.*; do /cvmfs/soft.computecanada.ca/easybuild/bin/setrpaths.sh --path %(installdir)s/mpi/latest/lib/$dir --add_path='$ORIGIN/../libfabric/lib'; done; "
-        "  for f in %(installdir)s/mpi/latest/lib/mpi/debug/*.so.*.*; do /cvmfs/soft.computecanada.ca/easybuild/bin/setrpaths.sh --path %(installdir)s/mpi/latest/lib/$dir --add_path='$ORIGIN/../../../libfabric/lib'; done; else"
+        "  for f in %(installdir)s/mpi/latest/lib/*.so.*.*; do /cvmfs/soft.computecanada.ca/easybuild/bin/setrpaths.sh --path $f --add_path='$ORIGIN/../libfabric/lib'; done; "
+        "  for f in %(installdir)s/mpi/latest/lib/mpi/debug/*.so.*.*; do /cvmfs/soft.computecanada.ca/easybuild/bin/setrpaths.sh --path $f --add_path='$ORIGIN/../../../libfabric/lib'; done; else"
         "  for dir in release debug; do /cvmfs/soft.computecanada.ca/easybuild/bin/setrpaths.sh --path %(installdir)s/mpi/latest/lib/$dir --add_path='$ORIGIN/../../libfabric/lib'; done; fi",
         "patchelf --set-rpath $EBROOTUCX/lib --force-rpath %(installdir)s/mpi/latest/libfabric/lib/prov/libmlx-fi.so",
         "patchelf --set-rpath $EBROOTOPAMINPSM2/lib64 --force-rpath %(installdir)s/mpi/latest/libfabric/lib/prov/libpsmx2-fi.so",
