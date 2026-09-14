84380eb024c9afabcc025230d1ff14b2679ab8f8
Charles Coulombe <charles.coulombe@calculquebec.ca>
2026-09-14 11:37:20 -0400

    Updated numpy version in the hooks for 2026

--- 2026/cc_hooks.py
+++ 2026/cc_hooks.py
@@ -145,8 +145,8 @@ def modify_list_of_dependencies(ec, param, version_mapping, list_of_deps):
             dep = new_dep
             new_dep = dep[:2]
         elif dep_name == 'oldest-supported-numpy':
-            print(f"Dependency on oldest-supported-numpy is now deprecated. Replacing with ('numpy', '2.1.1')")
-            new_dep = ('numpy', '2.1.1')
+            print(f"Dependency on oldest-supported-numpy is now deprecated. Replacing with ('numpy', '2.5.3')")
+            new_dep = ('numpy', '2.5.3')
         else:
             new_dep = None
         if new_dep is not None and str(new_dep) != str(dep):
